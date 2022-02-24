e21e6258b5b5c4d01628baf123e6506343ab2bfc
Huy <windinheart99@gmail.com>
2022-02-24 09:24:05 +0700

    update_code

--- test.cpp
+++ test.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <string>
+
+#define N 1000
 
 using namespace std;
 
-inline string fullName(const string &firstName, const string &middleName, const string &familyName)
+typedef struct Info
 {
-    return familyName+" "+middleName+" "+firstName;
+    string name;
+    int age; 
+
+    void printInfo(string name, int age)
+    {
+        cout<<"Your name is "<<name<<", you are "<<age<<"!"<<endl;
+    }
+};
+
+void outputArr(char* arr)
+{
+   
+    for (size_t i = 0; i < 3; i++)
+    {
+        /* code */
+
+        cout<<arr[i];
+    }
+    cout<<endl; 
 }
 
 int main()
 {
-    string firstName ="", middleName ="";
+    Info info;
 
-    cout<<"Input your middle & first name here: "<<endl;
-    cin>>middleName>>firstName;
+    char* arr= new char[100]; 
 
-    cout<<"Your full name is: "<<fullName(firstName,middleName,"Nguyen")<<endl;
+    cout<<"Input your name: "<<endl;
+    getline(cin,info.name);
+
+    cout<<"Input your age: "<<endl;
+    cin>>info.age;
+    
+    info.printInfo(info.name,info.age);
+
+
+    cout<<"Input your first name: "<<endl;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        /* code */
+        cin>>arr[i];
+    }
+    
+    outputArr(arr);
 
     return 0;
 }
