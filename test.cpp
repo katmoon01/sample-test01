115a72f1ac8696c63675c2366ad4170b6e2ad498
Huy <windinheart99@gmail.com>
2022-02-24 19:43:13 +0700

    pointer and 2D-array

--- test.cpp
+++ test.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
-#include <string>
 
-#define N 1000
+#define N 10
 
 using namespace std;
 
-typedef struct Info
+void showResult(int** arr)
 {
-    string name;
-    int age; 
-
-    void printInfo(string name, int age)
-    {
-        cout<<"Your name is "<<name<<", you are "<<age<<"!"<<endl;
-    }
-};
-
-void outputArr(char* arr)
-{
-   
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < 10; i++)
     {
         /* code */
-
-        cout<<arr[i];
+        for (size_t j = 0; j < 10; j++)
+        {
+            /* code */
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<endl;
     }
-    cout<<endl; 
+    
 }
 
 int main()
 {
-    Info info;
+    int** ptr = new int*[N];
 
-    char* arr= new char[100]; 
-
-    cout<<"Input your name: "<<endl;
-    getline(cin,info.name);
-
-    cout<<"Input your age: "<<endl;
-    cin>>info.age;
-    
-    info.printInfo(info.name,info.age);
-
-
-    cout<<"Input your first name: "<<endl;
-
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < 10; i++)
     {
         /* code */
-        cin>>arr[i];
+        ptr[i] = new int[N]; 
+    }
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        /* code */
+        for (size_t j = 0; j < 10; j++)
+        {
+            /* code */
+            *(*(ptr+i)+j) = j+i*10;
+        }
+        
     }
     
-    outputArr(arr);
-
+    showResult(ptr);
+    
     return 0;
 }
